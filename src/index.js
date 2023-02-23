e118f14e93932c133ecfe821bf06cfff30566896
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-02-23 15:21:26 +0530

    MVP Setup Done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+
+const server = () => {
+    
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on PORT : ${PORT}`);
+    });
+
+}
+
+server();
